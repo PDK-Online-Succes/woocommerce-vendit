10779a786bb32f8767fa5a9b5acd22fd8182f67d
Luuk de Bresser <info@luukdebresser.nl>
2025-01-31 14:22:34 +0100

    Changes to be committed: 	new file:   index.php 	new file:   woocommerce-vendit.php

---
+++ woocommerce-vendit.php
@@ -0,0 +1,12 @@
+/**
+ * Plugin Name:       Woocommerce Vendit 
+ * Plugin URI:        https://github.com/pdk-online-succes/woocommerce-vendit
+ * Description:       Woocommerce Vendit Koppeling
+ * Version:           1.0.0
+ * Author:            Luuk de Bresser
+ * License:           MIT
+  * Domain Path:       /languages
+ * Text Domain:       woocommerce-vendit
+ * GitHub Plugin URI: https://github.com/pdk-online-succes/woocommerce-vendit
+ * GitHub Languages:  https://github.com/pdk-online-succes/woocommerce-vendit-translations
+ */
